b95ff31d8767f7995e365d18a849bd4070a27fc5
apigituser <responsemail850@gmail.com>
2025-05-31 00:43:35 +0530

    json parameters and some redis

--- weather.py
+++ weather.py
@@ -2,10 +2,8 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-import redis
-import requests
-import json
-import os
+import redis, redis.exceptions
+import requests, datetime, os
 
 app = Flask(__name__)
 
@@ -17,27 +15,44 @@ limiter = Limiter(
 )
 
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
-
-@app.route("/gocrazy")
-@limiter.exempt
-def gocrazy():
-    return "RAAAAAAARRRRRRRRRR"
-
-@app.route("/check", methods=['GET'])
-@limiter.limit("10 per minute")
-def check():
-    return "10 requests per minute ONLY"
+try:
+    r.get('hash')
+except redis.exceptions.ConnectionError as e:
+    print('Redis ConnectionError: Couldn\'t Connect To The Server\n')
+    exit(1)
+except Exception as internal_error:
+    raise internal_error
 
 @app.route("/<location>", methods=['GET'])
 def weatherData(location):
     load_dotenv(dotenv_path="key.env")
 
     key = os.getenv('KEY')
-    date1 = "2025-05-30"
-    query = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date1}?key={key}"
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    query = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date}?include=current&key={key}"
     response = requests.get(query)
 
-    with open("WEATHER_DATA.json", "w") as file:
-        data = response.json()
-        json.dump(data, file)
-    return data
+    if response.status_code == 400:
+        return {"BadRequest": "Location Not Found"}
+    jsonData = response.json()
+
+    days = jsonData['days'][0]
+    data = {'timezone': jsonData['timezone'],
+            'latitude': jsonData['latitude'],
+            'longitude': jsonData['longitude'],
+            'datetime': days['datetime'],
+            'currenttemp': days['temp'],
+            'mintemp': days['tempmin'],
+            'maxtemp': days['tempmax'],
+            'windspeed': days['windspeed'],
+            'winddirection': days['winddir'],
+            'conditions': days['conditions'],
+            'description': days['description'],
+            'sunrise': days['sunrise'],
+            'sunset': days['sunset']
+    }
+    
+    #r.set(location, str(data), ex=20)
+    redis_data = r.get(location)
+
+    return redis_data
